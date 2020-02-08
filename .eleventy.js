2457cb61ccdaaa78afa1557bad9c1e5549502acf
priyankamadhwal <priyanka0701m@gmail.com>
2020-02-08 19:22:59 +0530

    Add new members in team

--- .eleventy.js
+++ .eleventy.js
@@ -15,6 +15,7 @@ module.exports = function(eleventyConfig) {
     }
   });
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
   return {
     passthroughFileCopy: true
   };
