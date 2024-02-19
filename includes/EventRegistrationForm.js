2f172f4ed41bb9662cf217dc07bab6cceabc1815
Sudipto Ghosh <sudipto@jhatkaa.org>
2024-02-19 19:21:13 +0530

    add registration skeleton

---
+++ includes/EventRegistrationForm.js
@@ -0,0 +1,9 @@
+const EventRegistrationForm = () => {
+  return (
+    <>
+      <h1 className="text-white mt-12">Form</h1>
+    </>
+  )
+};
+
+export default EventRegistrationForm;
--- pages/register/index.js
+++ pages/register/index.js
@@ -1,19 +1,63 @@
+import PageLayout from "@/includes/PageLayout";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { Button } from "@material-tailwind/react";
+
+import data from "@/data";
+import Head from "next/head";
+import EventRegistrationForm from "@/includes/EventRegistrationForm";
 
 export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+  const {
+    site, contact
+  } = data;
+  const { data: session } = useSession();
+
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <PageLayout>
+      <Head>
+        <title>Event Registration | {site.title}</title>
+        <meta name="description" content={site.description} />
+      </Head>
+      <h1 className="title-page">Event Registration</h1>
+      <p className="text-white">
+        {
+          session ? (
+              <span>You are signed in as {session.user.email}. Not you? <button className="text-pink-300" onClick={() => signOut({redirect: false})}>Sign Out</button></span>
+            ) : (
+         <Button
+          size="md"
+          variant="filled"
+          color="white"
+          className="flex items-center gap-3 font-[spacemono] mx-auto"
+          onClick={() => signIn('google')}
+        >
+          <img src="https://docs.material-tailwind.com/icons/google.svg" alt="metamask" className="h-4 w-4" />
+          Continue with Google
+        </Button>
+      )
+        }
+      </p>
+
+      {
+        session && (
+          <EventRegistrationForm />
+        )
+      }
+    </PageLayout>
   )
 }
+
+// if (session) {
+//   return (
+//     <>
+//       Signed in as {session.user.email} <br />
+//       <button onClick={() => signOut({redirect: false})}>Sign out</button>
+//     </>
+//   )
+// }
+// return (
+//   <>
+//     Not signed in <br />
+//     <button onClick={() => signIn('google')}>Sign in</button>
+//   </>
+// )
