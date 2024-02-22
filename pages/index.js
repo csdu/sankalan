cd4d7539c691dee115a4d893ab9863d3889b15c8
Sudipto Ghosh <sudipto@jhatkaa.org>
2024-02-22 09:08:41 +0530

    ui res fix

--- pages/index.js
+++ pages/index.js
@@ -31,14 +31,13 @@ export default function Home() {
         <meta name="twitter:title" content={site.title} />
         <meta name="twitter:description" content={site.description} />
       </Head>
-      <Image 
+      {/* <Image 
         alt='lines' 
-        id="lines" 
         src={background}
-        fill
         priority={true}
+
         objectFit='cover'
-      />
+      /> */}
 
       <DotParticles particlesInit={true} particlesLoaded={particlesLoaded} />
 
@@ -66,9 +65,10 @@ export default function Home() {
         <Link href={'/faq'} className='p-2 bg-white shadow-ls border-[.25rem] border-black hover:text-white hover:bg-sankalan-green transition hover:scale-110 duration-300 ease-in-out'>questions?</Link>
       </div>
 
-      <Link href={'/terms'} className='hidden'>team</Link>
+      <Link href={'/terms'} className='hidden'>Privacy Policy</Link>
 
-      <div className="landing-content lg:w-3/4 mx-auto lg:gap-x-20">
+      <div className="landing-content bg-[url('/images/4845746.jpg')] bg-cover bg-center bg-no-repeat bg-fixed flex flex-col lg:flex-row justify-center
+       lg:w-3/4 mx-auto lg:gap-x-20 min-h-screen lg:min-h-screen overflow-auto items-center">
         {/* <div className="lg:absolute z-10">
           <div className='date'>
            
@@ -76,35 +76,33 @@ export default function Home() {
           <div className="coordinates"><Link target="_blank" href={contact.googleLocation}>{ contact.coordinates }</Link></div>
         </div> */}
 
-        <div className="middle-content lg:w-1/2">
-          <div className="mx-auto">
-              <Image 
-                src={logo}
-                alt={site.title}
-                width={500}
-                height={500}
-                priority={true}
-                className='w-2/3 lg:w-auto mx-auto cursor-pointer transform hover:scale-105 transition-transform duration-300 ease-in-out'
-              />
-              <a className="inline-block bg-gradient-to-r from-sankalan-dark-yellow to-sankalan-accent-red
-               p-2 p-y-0 text-black hover:bg-sankalan-accent-yellow text-white mt-6
-              hover:scale-110 transition-transform transition-colors duration-300 ease-in-out border-4 border-white font-[spacemono] text-xl font-bold
-              lg:text-2xl lg:p-3 tracking-narrower
-              ">
-                <span 
-                  style={
-                    {
-                      top: '-1px',
-                      position: 'relative'
-                    }
+        <div className="">
+            <Image 
+              src={logo}
+              alt={site.title}
+              width={500}
+              height={500}
+              priority={true}
+              className='w-2/3 lg:w-auto mx-auto cursor-pointer transform hover:scale-105 transition-transform duration-300 ease-in-out'
+            />
+            <a className="inline-block bg-gradient-to-r from-sankalan-dark-yellow to-sankalan-accent-red
+              p-2 p-y-0 text-black hover:bg-sankalan-accent-yellow text-white mt-6
+            hover:scale-110 transition-transform transition-colors duration-300 ease-in-out border-4 border-white font-[spacemono] text-xl font-bold
+            lg:text-2xl lg:p-3 tracking-narrower
+            ">
+              <span 
+                style={
+                  {
+                    top: '-1px',
+                    position: 'relative'
                   }
-                  className=''
-                >
-                  COMING SOON<br />
-                  <Countdown />
-                </span>
-              </a>
-          </div>
+                }
+                className=''
+              >
+                COMING SOON<br />
+                <Countdown />
+              </span>
+            </a>
         </div>
 
         {/* <div className="right-content lg:w-1/4 mx-10">
