a900862222d8f2326d828595c0319ad314383ac0
Sudipto Ghosh <sudipto@jhatkaa.org>
2024-02-23 08:02:04 +0530

    move analytics to doc layout

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import "@/styles/main.css"
 import "@/styles/landing.css"
 
 import Head from 'next/head';
-import { Analytics } from '@vercel/analytics/react';
 import { SessionProvider } from "next-auth/react"
 import { createGlobalStyle } from "styled-components";
 import { config, dom } from "@fortawesome/fontawesome-svg-core";
@@ -45,7 +44,6 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
         <GlobalStyles />
         <Component {...pageProps}  />
       </SessionProvider>
-      <Analytics />
     </>
   )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-sync-scripts */
+import { Analytics } from "@vercel/analytics/react";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -10,6 +11,7 @@ export default function Document() {
         <NextScript />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <Analytics />
       </body>
     </Html>
   );
