a29e62bc80edc9a013bef05b201f1d879a95b1b3
Prakhar Verma <prakharverma1022@gmail.com>
2024-02-25 15:00:06 +0530

    removed one team member

--- pages/about/index.js
+++ pages/about/index.js
@@ -24,7 +24,7 @@ const About = () => {
         <meta name="twitter:description" content={site.description} />
       </Head>
       <h1 className='text-black text-2xl title-page'>About</h1>
-      <div className='container mx-auto text-white text-left lg:text-justify flex flex-col gap-4 mt-10'>
+      <div className='container mx-auto text-white text-justify flex flex-col gap-4 mt-10 md:px-6'>
         <p>SANKALAN is the illustrious annual fest organized by the Department of Computer Science, University of Delhi. Embarking on its 20th voyage, this two-day extravaganza epitomizes a diverse array of technical marvels and non-technical wonders to discover. It beckons both seasoned experts and newcomers to share in its brilliance. From riveting coding competitions to captivating artistic events, Sankalan offers everyone a platform to showcase their talents and explore the frontiers of technology and art. </p>
 
         <p>Amidst the joyful festivities, attendees will find themselves enveloped in an aura of creativity and community, forging lasting memories and fostering new connections. Join us as we celebrate two decades of excellence and embark on a journey to shape the future at Sankalan, where dreams transcend into reality and innovation knows no bounds.</p>
